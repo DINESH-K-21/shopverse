c6b660923c1036f41248a6c780923555aa84e446
dinesh <dineshverse21@gmail.com>
2026-06-18 12:09:58 +0530

    login and sign up page creation

--- frontend/my-app/src/pages/SignUp.jsx
+++ frontend/my-app/src/pages/SignUp.jsx
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import { Eye, EyeOff, Mail, Lock, LineStyle } from "lucide-react";
 import { Link } from "react-router-dom";
 
-export default function Login() {
+function SignUp() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
     e.stopPropagation();
-    console.log(email, password);
+    console.log(name, email, password);
   }
 
   return (
@@ -18,16 +19,37 @@ export default function Login() {
         <div className="bg-slate-800 rounded-2xl shadow-2xl overflow-hidden border border-slate-700 w-full h-auto">
           <div className="relative h-2 bg-gradient-to-r from-teal-500 via-cyan-500 to-blue-500"></div>
 
-          <div className="text-white text-[24px] font-bold text-center mt-1">SIGN IN</div>
+          <div className="text-white text-[24px] font-bold text-center mt-1">
+            SIGN UP
+          </div>
 
           <form className="w-full flex flex-col gap-4 px-6 pb-6 pt-3">
             <div>
-              <label htmlFor="email" className="text-white text-[14px] font-semibold">
+              <label
+                htmlFor="name"
+                className="text-white text-[14px] font-semibold"
+              >
+                Name
+              </label>
+
+              <input
+                id="name"
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full px-4 py-2 rounded-lg bg-slate-700 text-white placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-teal-500 mt-2"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="text-white text-[14px] font-semibold"
+              >
                 Email
               </label>
-              
+
               <input
-               
                 type="text"
                 placeholder="Email"
                 value={email}
@@ -36,7 +58,10 @@ export default function Login() {
               />
             </div>
             <div>
-              <label htmlFor="password" className="text-white text-[14px] font-semibold">
+              <label
+                htmlFor="password"
+                className="text-white text-[14px] font-semibold"
+              >
                 Password
               </label>
               <input
@@ -48,7 +73,15 @@ export default function Login() {
               />
             </div>
             <div>
-             < span className="text-white text-[14px] font-semibold">doesn't Signed yet ?</span> <Link to="/signup" className="text-teal-500 text-[14px] font-semibold ml-1">Sign Up</Link>
+              <span className="text-white text-[14px] font-semibold">
+                Already Signed In ?
+              </span>{" "}
+              <Link
+                to="/login"
+                className="text-teal-500 text-[14px] font-semibold ml-1"
+              >
+                Sign In
+              </Link>
             </div>
             <div>
               <button
@@ -56,7 +89,7 @@ export default function Login() {
                 onClick={handleSubmit}
                 className="w-full p-2 rounded-lg text-white font-semibold focus:ring-teal-500 bg-teal-500 hover:bg-teal-600 cursor-pointer"
               >
-                Login
+                SIGN UP
               </button>
             </div>
           </form>
@@ -65,3 +98,4 @@ export default function Login() {
     </div>
   );
 }
+export default SignUp;
