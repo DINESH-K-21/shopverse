6a1913b46434b58413411bffa979b2a925b663c8
dinesh <dineshverse21@gmail.com>
2026-06-28 21:14:26 +0530

    port settings

--- frontend/my-app/src/api/api.js
+++ frontend/my-app/src/api/api.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
-export const api = axios.create({baseURL: "http://localhost:3000/"});
- export const productApi = axios.create({baseURL: "http://localhost:4000/"});
+
+ export const api = axios.create({
+  baseURL: import.meta.env.VITE_AUTH_API,
+});
+
+export const productApi = axios.create({
+  baseURL: import.meta.env.VITE_PRODUCT_API,
+});
+
+console.log(import.meta.env.VITE_AUTH_API);
+console.log(import.meta.env.VITE_PRODUCT_API);
 
 api.interceptors.request.use(
   (config) => {
