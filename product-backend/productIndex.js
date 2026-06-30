703b6d9b0342ff06183387cc25a610a71a7ba5f2
dinesh <dineshverse21@gmail.com>
2026-06-30 15:04:09 +0530

    env changes

--- product-backend/productIndex.js
+++ product-backend/productIndex.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.send(`product-service is running on PORT ${PORT}!`);
 });
 
-app.use("/api/products", ProductRoutes);
+app.use("/", ProductRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
